f33522a95fbe4f6c7ec586e5455114585c6d7f99
artemsvetkin <artem.svetkin@visum.io>
2024-11-15 11:27:26 +0700

    Returned default timeout on the clipboard.component.ts

--- frontend/src/app/components/clipboard/clipboard.component.ts
+++ frontend/src/app/components/clipboard/clipboard.component.ts
@@ -33,11 +33,8 @@ export class ClipboardComponent implements AfterViewInit {
     this.clipboard.on('success', () => {
       tlite.show(this.buttonWrapper.nativeElement);
       setTimeout(() => {
-        console.log(document.querySelector('.title-n'));
-      }, 2000)
-      setTimeout(() => {
         tlite.hide(this.buttonWrapper.nativeElement);
-      }, 100000);
+      }, 1000);
     });
   }
 
